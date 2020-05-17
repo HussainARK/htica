7f8d16881b7ecc2fda229460434671574aecb65b
HussainA100 <58083845+HussainA100@users.noreply.github.com>
2020-05-17 20:44:33 +0300

    Initial Commiy

---
+++ client.py
@@ -0,0 +1,46 @@
+# Event-Driven Programming
+# # Here we're creating the client Script that connects to the Server
+
+import socket, threading
+
+DISCONNECT_MESSAGE ="!DISCONNECT!"
+HEADER = 2048
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 5050
+SERVER_ADDR = (SERVER_HOST, SERVER_PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(SERVER_ADDR)
+print(f"[CLIENT] CONNECTED TO THE SERVER {SERVER_ADDR}")
+
+def send(msg, sender_name):
+    sender_name = str.encode(sender_name)
+    client.send(sender_name)
+    message = str.encode(msg)
+    client.send(message)
+
+def get_messages():
+    messages = client.recv(HEADER).decode()
+    print("\n" + messages)
+
+sender = input('Enter your Name: ')
+
+print(f'\n[CLIENT] to disconnect enter "{DISCONNECT_MESSAGE}" (without quotes)\n')
+
+while True:
+    threading.Thread(target=get_messages).start()
+    message = input(f"{sender}> ")
+    threading.Thread(target=get_messages).start()
+    if message.strip() == "":
+        threading.Thread(target=get_messages).start()
+        pass
+    else:
+        threading.Thread(target=get_messages).start()
+        if message == DISCONNECT_MESSAGE:
+            send(message, sender)
+            print(f"[CLIENT] DISCONNECTED to the SERVER {SERVER_ADDR}")
+            exit()
+        else:
+            threading.Thread(target=get_messages).start()
+            send(message, sender)
---
+++ server.py
@@ -0,0 +1,53 @@
+# Event-Driven Programming
+# # Example, Here we're creating a socket Server that clients can connect to and send messages to each other
+
+import socket, threading, pickle
+
+DISCONNECT_MESSAGE ="!DISCONNECT!"
+HEADER = 2048
+
+HOST = "127.0.0.1"
+PORT = 5050
+ADDR = (HOST, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+clients = set()
+clients_lock = threading.Lock()
+
+def handle_client(connection, address):
+    print(f"[SERVER] New Connection: {address}")
+    with clients_lock:
+        clients.add(connection)
+
+    connected = True
+    while connected:
+        username = connection.recv(HEADER).decode()
+        msg = connection.recv(HEADER).decode()
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+            with clients_lock:
+                if clients != []:
+                    for client in clients:
+                        if client:
+                            client.send(str.encode(f"[{username} DISCONNECTED]" + "\n"))
+            print(f"[{username}@{address[0]}] DISCONNECTED")
+        else:
+            with clients_lock:
+                for client in clients:
+                    client.send(str.encode(f"[{username}] {msg}\n"))
+            print(f"[{username}@{address[0]} NEW MSG] {msg}")
+    connection.close()
+
+def start():
+    server.listen()
+    print(f"[SERVER] Listening on {ADDR}")
+    while True:
+        conn, addr = server.accept()
+        threading.Thread(target=handle_client, args=(conn, addr)).start()
+        print(f"[SERVER] Active Connections: {threading.activeCount() - 1}")
+
+
+print('[SERVER] HTICA Server is starting...')
+start()
